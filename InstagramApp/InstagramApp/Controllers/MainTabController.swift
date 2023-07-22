15be5d847d12d3bcd56e3c2ca1fef63ab8c3d49f
asiuzzaman <zaman1304006@gmail.com>
2023-07-22 22:48:31 +0600

    Set RootViewController from sceneDelegate

---
+++ InstagramApp/InstagramApp/Controllers/MainTabController.swift
@@ -0,0 +1,16 @@
+//
+//  MainTabController.swift
+//  InstagramApp
+//
+//  Created by Md. Asiuzzaman on 22/7/23.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
