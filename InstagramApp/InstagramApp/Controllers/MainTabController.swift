bc700eae175dade5609181151cc0177e02981898
asiuzzaman <zaman1304006@gmail.com>
2023-07-22 23:56:25 +0600

    finished all tabs

--- InstagramApp/InstagramApp/Controllers/MainTabController.swift
+++ InstagramApp/InstagramApp/Controllers/MainTabController.swift
@@ -16,11 +16,46 @@ class MainTabController: UITabBarController {
     
     func configureViewControllers() {
         view.backgroundColor = .white
-        let feed = FeedController()
-        let search = SearchController()
-        let image = ImageSelectorController()
-        let notifications = NotificationController()
-        let profie = ProfileController()
+        
+        let feed = templateNavigationController(
+            unselectedImage: UIImage(imageLiteralResourceName: "home_unselected"),
+            selectedImage: UIImage(imageLiteralResourceName: "home_selected"),
+            viewController: FeedController()
+        )
+        let search = templateNavigationController(
+            unselectedImage: UIImage(imageLiteralResourceName: "search_unselected"),
+            selectedImage: UIImage(imageLiteralResourceName: "search_selected"),
+            viewController: SearchController()
+        )
+        let image = templateNavigationController(
+            unselectedImage: UIImage(imageLiteralResourceName: "plus_unselected"),
+            selectedImage: UIImage(imageLiteralResourceName: "plus_unselected"),
+            viewController: ImageSelectorController()
+        )
+        let notifications = templateNavigationController(
+            unselectedImage: UIImage(imageLiteralResourceName: "like_unselected"),
+            selectedImage: UIImage(imageLiteralResourceName: "like_selected"),
+            viewController: NotificationController()
+        )
+        let profie = templateNavigationController(
+            unselectedImage: UIImage(imageLiteralResourceName: "profile_unselected"),
+            selectedImage: UIImage(imageLiteralResourceName: "profile_selected"),
+            viewController: ProfileController()
+        )
+        
         viewControllers = [feed, search, image, notifications, profie]
+        tabBar.tintColor = .black
+    }
+    
+    func templateNavigationController(
+        unselectedImage: UIImage,
+        selectedImage: UIImage,
+        viewController: UIViewController) -> UINavigationController
+    {
+        let nav = UINavigationController(rootViewController: viewController)
+        nav.tabBarItem.image = unselectedImage
+        nav.tabBarItem.selectedImage = selectedImage
+        nav.navigationBar.tintColor = .black
+        return nav
     }
 }
