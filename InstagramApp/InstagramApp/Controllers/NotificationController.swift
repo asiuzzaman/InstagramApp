644d88e0172cca85e9de1a81be56ac011704a349
asiuzzaman <zaman1304006@gmail.com>
2023-08-06 12:43:21 +0600

    initiate UI for notification cell

--- InstagramApp/InstagramApp/Controllers/NotificationController.swift
+++ InstagramApp/InstagramApp/Controllers/NotificationController.swift
@@ -7,10 +7,32 @@
 
 import UIKit
 
-class NotificationController: UIViewController {
+private let reuseIdentifier = "NotificationCell"
+
+class NotificationController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemYellow
+        configureUITableView()
+        
+    }
+    
+    func configureUITableView() {
+        view.backgroundColor = .white
+        navigationItem.title = "Notifications"
+        tableView.register(NotificationCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 80
+        tableView.separatorStyle = .none
+    }
+}
+
+extension NotificationController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! NotificationCell
+        return cell
     }
 }
---
+++ InstagramApp/InstagramApp/View/NotificationCell.swift
@@ -0,0 +1,89 @@
+//
+//  NotificationCell.swift
+//  InstagramApp
+//
+//  Created by Md. Asiuzzaman on 6/8/23.
+//
+
+import UIKit
+
+
+class NotificationCell: UITableViewCell {
+    
+    
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.isUserInteractionEnabled = true
+        imageView.image = UIImage(imageLiteralResourceName: "venom-7")
+        imageView.backgroundColor = .lightGray
+        return imageView
+    }()
+    
+    private let infoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "zaman"
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    private let postImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .lightGray
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handlePostTap))
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(tap)
+        return imageView
+    }()
+    
+    private let followButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Loading", for: .normal)
+        button.layer.cornerRadius = 3
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        button.layer.borderWidth = 0.5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(handleFollowButton), for: .touchUpInside)
+        return button
+    }()
+    
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        selectionStyle = .none
+        addSubview(profileImageView)
+        profileImageView.setDimensions(height: 48, width: 48)
+        profileImageView.layer.cornerRadius = 48 / 2
+        profileImageView.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 12)
+        
+        addSubview(infoLabel)
+        infoLabel.centerY(inView: profileImageView, leftAnchor: profileImageView.rightAnchor, paddingLeft: 8)
+        
+        addSubview(followButton)
+        followButton.centerY(inView: self)
+        followButton.anchor(right: rightAnchor, paddingRight: 12, width: 100, height: 32 )
+        
+        addSubview(postImageView)
+        postImageView.centerY(inView: self)
+        postImageView.anchor(right: rightAnchor, paddingRight: 12, width: 40, height: 40 )
+        
+        followButton.isHidden = true
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func handleFollowButton() {
+        
+    }
+    
+    @objc func handlePostTap() {
+        
+    }
+}
